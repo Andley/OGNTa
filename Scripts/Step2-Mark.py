e0e3d7af02c33bd0e4228b0c1ccd78b63b809a96
Andley Chang <andley@gmail.com>
2023-09-04 06:48:16 +0800

    update OGNTA-TC-marked.txt & OGNTa-TC-Ruby.nt

--- Scripts/Step2-Mark.py
+++ Scripts/Step2-Mark.py
@@ -50,8 +50,7 @@ f1 = open(outputFile1,'w',encoding="utf_8_sig")
 for line in Lines:
 	x = re.split("\t", line)
 
-
-	x[2] = re.sub(r'([\.,;·])',r' <mark class="pm">\1</mark>',x[2])
+	x[2] = re.sub(r'([\.,;·])',r' <mark class="pm">\1</mark>',x[2])
 	# print (x[2])
 
 	# add emphasis for Verbs
